57a2c34e907d4741b41fc40a128d61d3f44d2323
Ensoul <jbgong@whu.edu.cn>
2020-08-15 15:52:55 +0800

    table

--- 451.Sort Characters By Frequency/solution.h
+++ 451.Sort Characters By Frequency/solution.h
@@ -37,3 +37,28 @@ public:
 };
  
  
+//3. 优先队列，priority_queue<pair<int, char>> pq
+//首先按第一个值的大小，从大到小排列，相等则比较第二个元素
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> ump;
+        for (const auto &c : s) {
+            ++ump[c];
+        }
+        priority_queue<pair<int, char>> pq;
+        for (const auto &m : ump) {
+            pq.push({m.second, m.first});
+        }        
+        string ret;
+        while (!pq.empty()) {
+            auto t = pq.top(); 
+            pq.pop();
+            ret.append(t.first, t.second);
+        }
+        return ret;
+    }
+};
+
+
+ 
